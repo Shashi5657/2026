bc7a7a7bb3d7d46de7ce20be3f92fd7845e52138
shashi5657 <sangepushashidhar@gmail.com>
2026-01-12 16:46:47 +0530

    nest learning curve

--- nestjs-beginner/src/app.module.ts
+++ nestjs-beginner/src/app.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
 import { catsService } from './cats/cats.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService, catsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('cats');
+  }
+}
--- nestjs-beginner/src/cats/cats.module.ts
+++ nestjs-beginner/src/cats/cats.module.ts
@@ -1,4 +1,28 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { catsService } from './cats.service';
+import { CatsController } from './cats.controller';
 
-@Module({})
+@Module({
+  controllers: [CatsController],
+  providers: [catsService],
+})
 export class CatsModule {}
+
+//dynamic module
+// @Module({})
+// export class DatabaseModule{
+//     static forRoot(options): DynamicModule{
+//         return {
+//             module: DatabaseModule,
+//             providers: [
+//                 {
+//                     provide: 'DB_OPTIONS',
+//                     useValue: options
+//                 },
+//                 DatabaseService,
+//             ],
+//             exports: [DatabaseService]
+//         }
+
+//     }
+// }
