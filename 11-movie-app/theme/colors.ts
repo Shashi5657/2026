bdde18b210707a1cc055b7e105bf6336fcef643c
Shashi5657 <sangepushashidhar@gmail.com>
2026-02-04 15:20:28 +0530

    react native second project

---
+++ 11-movie-app/theme/colors.ts
@@ -0,0 +1,20 @@
+export const COLORS = {
+  primary: "#E50914",
+
+  background: "#0B0F14",
+  surface: "#11161D",
+  card: "#181F29",
+
+  textPrimary: "#FFFFFF",
+  textSecondary: "#9CA3AF",
+  textMuted: "#6B7280",
+
+  border: "#202833",
+  overlay: "rgba(0,0,0,0.75)",
+
+  success: "#22C55E",
+  warning: "#F59E0B",
+  error: "#EF4444",
+
+  accent: "#F5C518",
+};
