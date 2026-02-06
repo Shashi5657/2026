61ef48888ea7e3aa59af55bb01456a1b684e60fb
Shashi5657 <sangepushashidhar@gmail.com>
2026-02-06 15:21:34 +0530

    react native added themes

---
+++ 11-movie-app/theme/shadows.ts
@@ -0,0 +1,23 @@
+export const SHADOWS = {
+  card: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+
+  button: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 6,
+    elevation: 3,
+  },
+};
