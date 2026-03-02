9a8111766ed5f630aaf1f5fbdd127ab3a29fd11f
Shashi5657 <sangepushashidhar@gmail.com>
2026-03-02 09:13:47 +0530

    project 13

--- 13-project/backend/src/app.ts
+++ 13-project/backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(errorHandler);
 
 app.get("/health", (_, res) => {
   res.json({
---
+++ 13-project/backend/src/utils/sendResponse.ts
@@ -0,0 +1,20 @@
+import { Response } from "express";
+
+interface ResponseOptions<T> {
+  statusCode: number;
+  success: boolean;
+  message: string;
+  data: T;
+  meta?: Record<string, unknown>;
+}
+
+export const sendResponse = <T>(res: Response, options: ResponseOptions<T>) => {
+  const { statusCode, success, message, data, meta } = options;
+
+  return res.status(statusCode).json({
+    success,
+    message,
+    data,
+    meta,
+  });
+};
