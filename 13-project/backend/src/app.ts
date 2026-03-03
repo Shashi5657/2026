87f18d17079e828faca694db5118b9a5ebe5743d
Shashi5657 <sangepushashidhar@gmail.com>
2026-03-03 10:19:21 +0530

    project 13

--- 13-project/backend/src/app.ts
+++ 13-project/backend/src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import { errorHandler } from "./middlewares/errorHandler";
+import { swaggerSpec } from "./config/swagger";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 
@@ -11,8 +13,19 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.use(errorHandler);
 
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Health Check
+ *     responses:
+ *       200:
+ *         description: Server is healthy
+ */
 app.get("/health", (_, res) => {
   res.json({
     success: true,
