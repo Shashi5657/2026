f2308dc6ce09dc587a9bfe2ad1dd69b9b3c52760
Shashi5657 <sangepushashidhar@gmail.com>
2026-03-13 12:31:06 +0530

    react native theming

---
+++ 13-project/frontend/src/theme/typography.ts
@@ -0,0 +1,36 @@
+export const typography = {
+  h1: {
+    fontSize: 32,
+    fontWeight: "700",
+  },
+
+  h2: {
+    fontSize: 28,
+    fontWeight: "700",
+  },
+
+  h3: {
+    fontSize: 24,
+    fontWeight: "600",
+  },
+
+  bodyLarge: {
+    fontSize: 18,
+    fontWeight: "400",
+  },
+
+  body: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+
+  bodySmall: {
+    fontSize: 14,
+    fontWeight: "400",
+  },
+
+  caption: {
+    fontSize: 12,
+    fontWeight: "400",
+  },
+};
